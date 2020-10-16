559ea3319d75d6edd9839b292bfb90007b6352cb
MIKEL <MIKEL@MIKEL>
2020-10-16 18:13:01 +0200

    bottones cambio de color

--- I-shop/src/iShop/ventanasPrincipales/VentanaLogin.java
+++ I-shop/src/iShop/ventanasPrincipales/VentanaLogin.java
@@ -9,6 +9,9 @@ import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.ImageIcon;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import java.awt.SystemColor;
 
 public class VentanaLogin {
 
@@ -44,39 +47,50 @@ public class VentanaLogin {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setIgnoreRepaint(true);
 		frame.setBounds(100, 100, 500, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JButton btnNewButton = new JButton("ENTRAR");
-		btnNewButton.setBounds(132, 363, 185, 48);
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setBackground(new Color(255, 165, 0));
+		btnNewButton.setBounds(132, 366, 185, 48);
 		frame.getContentPane().add(btnNewButton);
 		
 		JButton btnNuevoCliente = new JButton("NUEVO CLIENTE");
+		btnNuevoCliente.setForeground(new Color(255, 255, 255));
+		btnNuevoCliente.setBackground(new Color(192, 192, 192));
 		btnNuevoCliente.setBounds(132, 439, 185, 48);
 		frame.getContentPane().add(btnNuevoCliente);
 		
 		txtEmail = new JTextField();
+		txtEmail.setBackground(SystemColor.menu);
 		txtEmail.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		txtEmail.setText("EMAIL");
-		txtEmail.setBounds(105, 178, 236, 42);
+		txtEmail.setBounds(115, 179, 236, 42);
 		frame.getContentPane().add(txtEmail);
 		txtEmail.setColumns(10);
 		
 		txtContrase = new JTextField();
+		txtContrase.setBackground(SystemColor.menu);
+		txtContrase.setHorizontalAlignment(SwingConstants.LEFT);
 		txtContrase.setText("CONTRASE\u00D1A");
 		txtContrase.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		txtContrase.setColumns(10);
-		txtContrase.setBounds(105, 252, 236, 42);
+		txtContrase.setBounds(115, 252, 236, 42);
 		frame.getContentPane().add(txtContrase);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(105, 323, 236, 14);
+		JLabel lblNewLabel = new JLabel("He olvidado mi contrase\u00F1a");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setForeground(new Color(255, 165, 0));
+		lblNewLabel.setBounds(105, 319, 236, 14);
 		frame.getContentPane().add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setIcon(new ImageIcon("E:\\Deusto\\Proyectos\\Proyecto Prog 3\\fotos\\i-shopNombreLogo.jpg"));
-		lblNewLabel_1.setBounds(146, 47, 171, 94);
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon("C:/Users/MIKEL/git/I-Shop/I-shop/img/i-shopNombreLogo.jpg"));
+		lblNewLabel_1.setBounds(115, 47, 236, 109);
 		frame.getContentPane().add(lblNewLabel_1);
 	}
 }
