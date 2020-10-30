acc92d92a109e66fc16bb894f017958ca630c708
xabiergarai <xabiergarai@localhost>
2020-10-30 12:59:43 +0100

    VentanaCarrito, ventanaSeccion

--- I-shop/src/iShop/ventanasPrincipales/VentanaCarritoCompra.java
+++ I-shop/src/iShop/ventanasPrincipales/VentanaCarritoCompra.java
@@ -3,6 +3,10 @@ package iShop.ventanasPrincipales;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JCheckBox;
+import java.awt.Color;
 
 public class VentanaCarritoCompra {
 
@@ -36,8 +40,57 @@ public class VentanaCarritoCompra {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 400, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("Vaciar cesta");
+		btnNewButton.setBounds(104, 27, 212, 29);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Guardar lista");
+		btnNewButton_1.setBounds(104, 56, 212, 29);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Seguir comprando");
+		btnNewButton_2.setBounds(104, 85, 212, 29);
+		frame.getContentPane().add(btnNewButton_2);
+		
+		JLabel lblNewLabel = new JLabel("* Con I-ShoPremium puedes benificiarte de gastos");
+		lblNewLabel.setBounds(28, 126, 352, 29);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("de envio GRATIS!");
+		lblNewLabel_1.setBounds(28, 153, 130, 16);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JCheckBox chckbxNewCheckBox = new JCheckBox("Contratar tarifa premium");
+		chckbxNewCheckBox.setBounds(20, 179, 188, 23);
+		frame.getContentPane().add(chckbxNewCheckBox);
+		
+		JLabel lblNewLabel_2 = new JLabel("TOTAL                                                              999,99");
+		lblNewLabel_2.setBounds(20, 215, 360, 16);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JButton btnNewButton_3 = new JButton("REALIZAR PEDIDO");
+		btnNewButton_3.setBackground(Color.ORANGE);
+		btnNewButton_3.setBounds(104, 243, 188, 29);
+		frame.getContentPane().add(btnNewButton_3);
+		
+		JLabel lblNewLabel_3 = new JLabel("Formas de pago");
+		lblNewLabel_3.setBounds(28, 313, 108, 16);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		JCheckBox chckbxNewCheckBox_1 = new JCheckBox("VISA");
+		chckbxNewCheckBox_1.setBounds(20, 353, 83, 23);
+		frame.getContentPane().add(chckbxNewCheckBox_1);
+		
+		JCheckBox chckbxNewCheckBox_2 = new JCheckBox("MasterCard");
+		chckbxNewCheckBox_2.setBounds(113, 353, 119, 23);
+		frame.getContentPane().add(chckbxNewCheckBox_2);
+		
+		JCheckBox chckbxNewCheckBox_3 = new JCheckBox("PayPal");
+		chckbxNewCheckBox_3.setBounds(244, 353, 97, 23);
+		frame.getContentPane().add(chckbxNewCheckBox_3);
 	}
-
 }
--- I-shop/src/iShop/ventanasPrincipales/VentanaSeccion.java
+++ I-shop/src/iShop/ventanasPrincipales/VentanaSeccion.java
@@ -7,6 +7,9 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class VentanaSeccion {
 
@@ -40,7 +43,7 @@ public class VentanaSeccion {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(800, 800, 500, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -49,16 +52,36 @@ public class VentanaSeccion {
 		lblNewLabel.setBounds(95, 29, 224, 16);
 		frame.getContentPane().add(lblNewLabel);
 		
-		JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.setBounds(222, 93, 117, 29);
+		JButton btnNewButton_1 = new JButton("Accesorios");
+		btnNewButton_1.setBounds(274, 64, 117, 29);
 		frame.getContentPane().add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("New button");
-		btnNewButton_2.setBounds(128, 161, 117, 29);
+		JButton btnNewButton_2 = new JButton("Smartphone");
+		btnNewButton_2.setBounds(192, 213, 117, 29);
 		frame.getContentPane().add(btnNewButton_2);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(33, 93, 117, 29);
+		JButton btnNewButton = new JButton("PC");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(46, 64, 117, 29);
 		frame.getContentPane().add(btnNewButton);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setIcon(new ImageIcon("/Users/xabiergarai/Desktop/Uni/Prog III/Proyecto /Imagenes/pc .jpg"));
+		lblNewLabel_1.setBounds(64, 121, 99, 55);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setIcon(new ImageIcon("/Users/xabiergarai/Desktop/Uni/Prog III/Proyecto /Imagenes/accesorios.jpg"));
+		lblNewLabel_2.setBounds(315, 121, 61, 16);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		lblNewLabel_3.setBackground(Color.WHITE);
+		lblNewLabel_3.setIcon(new ImageIcon("/Users/xabiergarai/Desktop/Uni/Prog III/Proyecto /Imagenes/smartphone.png"));
+		lblNewLabel_3.setBounds(166, 244, 80, 16);
+		frame.getContentPane().add(lblNewLabel_3);
 	}
 }
