a1459b276f79bf9eab1027753e71aef2052840a2
xabiergarai <xabiergarai@localhost>
2020-10-31 12:48:31 +0100

    Test Usuario/Producto

---
+++ I-shop/src/Test/ProductoTest.java
@@ -0,0 +1,50 @@
+package Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+
+import Clases.Producto;
+
+class ProductoTest {
+	
+	@Test
+	public void testGetNombreProducto() {
+		
+		Producto p = new Producto("Macbook Pro", "E01","Atributos",999.99);
+		assertEquals("Macbook Pro",p.getNombre());	
+		
+	}
+	
+	@Test
+	public void testGetCodigoProducto() {
+		
+		Producto p = new Producto("Macbook Pro", "E01","Atributos",999.99);
+		assertEquals("E01",p.getCodigo());	
+		
+	}
+	
+	@Test
+	public void testGetAtributosProducto() {
+		
+		Producto p = new Producto("Macbook Pro", "E01","Atributos",999.99);
+		assertEquals("Atributos",p.getAtributos());	
+		
+	}
+	
+	@Test
+	public void testGetPrecioProducto() {
+		
+		Producto p = new Producto("Macbook Pro", "E01","Atributos",999.99);
+		assertEquals("999.99",p.getNombre());	
+		
+	}
+	
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ I-shop/src/Test/UsuarioTest.java
@@ -0,0 +1,48 @@
+package Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import Clases.Usuario;
+
+class UsuarioTest {
+
+	
+	@Test
+	public void testGetNomUsuario() {
+		Usuario u = new Usuario("XabierG","Contraeña1","Xabier.gaari@opendeusto.es","Xabier","Garai, Pascual");
+		assertEquals("XabierG", u.getNomUsuario());
+		
+	}
+	public void testgetNombre() {
+		Usuario u = new Usuario("XabierG","Contraeña1","Xabier.gaari@opendeusto.es","Xabier","Garai, Pascual");
+		assertEquals("Xabier", u.getNombre());
+	}
+	@Test
+	public void testGetApellido() {
+		Usuario u = new Usuario("XabierG","Contraeña1","Xabier.gaari@opendeusto.es","Xabier","Garai, Pascual");
+		assertEquals("Garai, Pascual", u.getApellidos());
+	
+	}
+	
+	@Test
+	public void testGetcontrasenya() {
+		Usuario u = new Usuario("XabierG","Contraeña1","Xabier.gaari@opendeusto.es","Xabier","Garai, Pascual");
+		assertEquals("Contraseña1", u.getContrasenya());
+		
+	}
+	
+	public void testGetEmail() {
+		Usuario u = new Usuario("XabierG","Contraeña1","Xabier.gaari@opendeusto.es","Xabier","Garai, Pascual");
+		assertEquals("Xabier.garai@opendesuto.es", u.getContrasenya());
+	}
+	
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
