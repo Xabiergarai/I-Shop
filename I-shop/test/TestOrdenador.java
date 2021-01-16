e2dd2abd1b49b5b1b4857625dd3f3cbdf301724f
baiko <baiko@192.168.0.27>
2021-01-16 15:01:04 +0100

    cambios test (eliminado atributo codigo)

--- I-shop/test/TestOrdenador.java
+++ I-shop/test/TestOrdenador.java
@@ -12,20 +12,20 @@ class TestOrdenador extends GroupException{
 
 	@Test
 	public void testGetRam() {
-		Ordenador o = new Ordenador("Macbook Pro Intel Core","Atributos","Intel", 999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
+		Ordenador o = new Ordenador("Macbook Pro Intel Core","Intel", 999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
 		assertEquals("DDR4 8GB*2 (2666MHz)", o.getRam());
 		
 	}
 	@Test
 	public void testGetPulgadas() {
-		Ordenador o = new Ordenador("Macbook Pro Intel Core","Atributos","Intel", 999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
+		Ordenador o = new Ordenador("Macbook Pro Intel Core","Intel", 999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
 		assertEquals("15.6 FHD (1920*1080)", o.getPulgadas());
 		
 	}
 			
 	@Test
 	public void testGetPortatil() {
-		Ordenador o = new Ordenador("Macbook Pro Intel Core","Atributos","Intel", 999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
+		Ordenador o = new Ordenador("Macbook Pro Intel Core","Intel", 999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
 		assertEquals(true, o.isPortatil());
 		
 	}
@@ -37,28 +37,26 @@ class TestOrdenador extends GroupException{
 	
 	public void testInvalidArguments() {
 		expected.expect(IllegalArgumentException.class);
-		new Ordenador("", "Intel","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
-		new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
-		new Ordenador("Macbook Pro Intel Core", "","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
-		new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",0.0,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
-		new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
-		new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
-		new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","","DDR4 8GB*2 (2666MHz)",15.6,true);
-		new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","",15.6,true);
-		new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",0.0,true);
-		new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,false);
+		new Ordenador("", "Intel", 999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
+		new Ordenador("Macbook Pro Intel Core","",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
+		new Ordenador("Macbook Pro Intel Core","Intel",0.0,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
+		new Ordenador("Macbook Pro Intel Core", "Intel",999.99,"","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
+		new Ordenador("Macbook Pro Intel Core", "Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
+		new Ordenador("Macbook Pro Intel Core", "Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","","DDR4 8GB*2 (2666MHz)",15.6,true);
+		new Ordenador("Macbook Pro Intel Core", "Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","",15.6,true);
+		new Ordenador("Macbook Pro Intel Core", "Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",0.0,true);
+		new Ordenador("Macbook Pro Intel Core", "Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,false);
 		
 		
-		new Ordenador(null, "Intel","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
-		new Ordenador("Macbook Pro Intel Core",null,"Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
-		new Ordenador("Macbook Pro Intel Core", "Atributos",null,99.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
-		new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",0.0,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
-		new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,null,"Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
-		new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial",null,"img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
-		new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador",null,"DDR4 8GB*2 (2666MHz)",15.6,true);
-		new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg",null,15.6,true);
-		new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",0.0,true);
-		new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,false);
+		new Ordenador(null, "Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
+		new Ordenador("Macbook Pro Intel Core",null,999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
+		new Ordenador("Macbook Pro Intel Core","Intel",0.0,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
+		new Ordenador("Macbook Pro Intel Core", "Intel",999.99,null,"Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
+		new Ordenador("Macbook Pro Intel Core", "Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial",null,"img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
+		new Ordenador("Macbook Pro Intel Core", "Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador",null,"DDR4 8GB*2 (2666MHz)",15.6,true);
+		new Ordenador("Macbook Pro Intel Core", "Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg",null,15.6,true);
+		new Ordenador("Macbook Pro Intel Core", "Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",0.0,true);
+		new Ordenador("Macbook Pro Intel Core", "Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,false);
 		
 		
 	
@@ -66,20 +64,20 @@ class TestOrdenador extends GroupException{
 	
 	public void testEquals() {
 		ArrayList<Ordenador> o = new ArrayList<>();
-		Ordenador sameOrdenador = new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
+		Ordenador sameOrdenador = new Ordenador("Macbook Pro Intel Core","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
 		assertTrue(o.equals(sameOrdenador));
 		
-		Ordenador differentOrdenador = new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
+		Ordenador differentOrdenador = new Ordenador("Macbook Pro Intel Core", "Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
 		assertFalse(o.equals(differentOrdenador));
 		
-		differentOrdenador = new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
+		differentOrdenador = new Ordenador("Macbook Pro Intel Core", "Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true);
 		assertFalse(o.equals(differentOrdenador));
 
 	}
 	
 	public void testAddOrdenador() throws GroupException {
 		ArrayList<Ordenador> o = new ArrayList<>();
-		o.add(new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true));
+		o.add(new Ordenador("Macbook Pro Intel Core","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true));
 		assertEquals(1, o.size());
 	}
 	
@@ -87,11 +85,11 @@ class TestOrdenador extends GroupException{
 	
 	public void testAddOrdenadorErrors() throws GroupException {
 		ArrayList<Ordenador> o = new ArrayList<>();
-		o.add(new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true));
+		o.add(new Ordenador("Macbook Pro Intel Core","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true));
 		assertEquals(1, o.size());
 		
 	   expected.expect(GroupException.class);
-	   o.add(new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true));
+	   o.add(new Ordenador("Macbook Pro Intel Core","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true));
 	   assertEquals(2, o.size());
 	   
 	   expected.expect(NullPointerException.class);
@@ -101,10 +99,10 @@ class TestOrdenador extends GroupException{
 	
 	public void testRemoveOrdenador() throws GroupException {
 		ArrayList<Ordenador> o = new ArrayList<>();
-		o.add(new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true));
+		o.add(new Ordenador("Macbook Pro Intel Core","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true));
 		assertEquals(1, o.size());
 		
-		o.remove(new Ordenador("Macbook Pro Intel Core", "Atributos","Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true));
+		o.remove(new Ordenador("Macbook Pro Intel Core", "Intel",999.99,"Macbook Pro Intel Core i5/8gb/512GB SSD/13.3 Gris Espacial","Ordenador","img/Portatil.jpg","DDR4 8GB*2 (2666MHz)",15.6,true));
 		assertEquals(0, o.size());
 			
 		}
